52095f379001e157296a88c6f4644201ffcb6e2c
Asus <mehhussumon47@gmail.com>
2025-02-08 22:20:40 +0600

    Initial Commit 3

--- app/src/main/java/com/example/learnnow/InstructorActivity.java
+++ app/src/main/java/com/example/learnnow/InstructorActivity.java
@@ -1,24 +1,37 @@
 package com.example.learnnow;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class InstructorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_instructor);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            if (item.getItemId() == R.id.nav_add_course) {
+                selectedFragment = new AddCourseFragment();
+            } else if (item.getItemId() == R.id.nav_request_course) {
+                selectedFragment = new RequestCourseFragment();
+            } else if (item.getItemId() == R.id.nav_details) {
+                selectedFragment = new InstructorDetailsFragment();
+            }
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+            return true;
         });
+
+        // Load default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AddCourseFragment()).commit();
+        }
     }
 }
